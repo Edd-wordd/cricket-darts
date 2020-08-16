c3750eaef0c48ab6803f9e6b34cc50d7d3019cb0
Edd-wordd <edward.plasencio@gmail.com>
2020-08-15 23:55:01 -0700

    add routes & pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import DrinkMenu from "./components/pages/DrinkMenu";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
    return (
-      <>
-         <h1 className="text-primary">Edward is here</h1>
-      </>
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/losers-drink" component={DrinkMenu}></Route>
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
    );
 }
 
---
+++ src/components/pages/DrinkMenu.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DrinkMenu(props) {
+   return <h1>Drink Menu Page</h1>;
+}
