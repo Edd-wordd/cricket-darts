00720b9523592e11d8b338244eb5e61abf159ee2
Edd-wordd <edward.plasencio@gmail.com>
2020-08-17 21:04:23 -0700

    add random drink function

--- src/components/pages/DrinkMenu.js
+++ src/components/pages/DrinkMenu.js
@@ -2,29 +2,71 @@ import React from "react";
 import leftArrow from "../../icons/arrow-thick-left.svg";
 import { Link } from "react-router-dom";
 
-export default function DrinkMenu(props) {
-   return (
-      <div className="body">
-         <div className="container">
-            <div className="row">
-               <div className="col-6 offset-3">
-                  <h1 className="text-center loser-page-title ">GOOD LUCK</h1>
+export default class DrinkMenu extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         drink: "",
+      };
+   }
+   getLoserDrink() {
+      console.log("drink clicked");
+      const drinkMenu = [
+         "water shot",
+         "whiskey shot",
+         "vodka shot",
+         "jager shot",
+         "beer shot",
+         "henny shot",
+         "malibu shot",
+         "loser picks",
+         "winner picks",
+         "tequilla shot",
+         "wine shot",
+         "moonshine shot",
+         "rum shot",
+         "no shot",
+         "milk shot",
+      ];
+      let randomDrink = drinkMenu[Math.floor(Math.random() * drinkMenu.length)];
+      console.log(randomDrink);
+      let newDrink = this.setState({ drink: randomDrink });
+      return newDrink;
+   }
+   render() {
+      return (
+         <div className="body">
+            <div className="container">
+               <div className="row">
+                  <div className="col-6 offset-3">
+                     <h1 className="text-center loser-page-title ">
+                        GOOD LUCK
+                     </h1>
 
-                  <button type="button" class="btn btn-danger btn-lg btn-block">
-                     What are you drinking?!
-                  </button>
+                     <button
+                        type="button"
+                        className="btn btn-danger btn-lg btn-block"
+                        onClick={() => {
+                           this.getLoserDrink();
+                        }}
+                     >
+                        What are you drinking?!
+                     </button>
+                  </div>
                </div>
-            </div>
-            <div className="row">
-               <div className="col-10 offset-1">
-                  <p className=" drink-name">WHISKEY SHOT!</p>
+               <div className="row">
+                  <div className="col-10 offset-1">
+                     <p className="drink-name" id="losers-drink">
+                        {this.state.drink}
+                     </p>
+                  </div>
                </div>
+               <Link to="/" className="btn btn-danger">
+                  <img src={leftArrow} className=" mr-2" alt="arrow-icon" />
+                  Play Again
+               </Link>
             </div>
-            <Link to="/" className="btn btn-danger">
-               <img src={leftArrow} className=" mr-2" />
-               Play Again
-            </Link>
          </div>
-      </div>
-   );
+      );
+   }
 }
