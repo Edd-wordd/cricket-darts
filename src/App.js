ebc51231672dae0462df24835655b7155a1fd0f1
Edd-wordd <edward.plasencio@gmail.com>
2020-09-03 21:41:21 -0700

    add rules page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import DrinkMenu from "./components/pages/DrinkMenu";
 import NotFound from "./components/pages/NotFound";
+import Rules from "./components/pages/Rules";
 
 function App() {
    return (
       <Router>
          <Switch>
+            <Route exact path="/rules" component={Rules}></Route>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/losers-drink" component={DrinkMenu}></Route>
             <Route component={NotFound} />
---
+++ src/components/pages/Rules.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Rules(props) {
+   return <h1>Rules Page</h1>;
+}
